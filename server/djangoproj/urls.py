92cef325c56b4a19973d600c0b4ce71f77f65885
Reyisreal <mofiyinolawuyi.acl@gmail.com>
2024-04-06 06:55:00 -0400

    Adding temporary changes to Github

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -24,5 +24,8 @@ urlpatterns = [
     path('djangoapp/', include('djangoapp.urls')),
     path('', TemplateView.as_view(template_name="Home.html")),
     path('about/', TemplateView.as_view(template_name="About.html")),
+    path('login/', TemplateView.as_view(template_name="index.html")),
+    path('dealers/', TemplateView.as_view(template_name="index.html")),
+    path('register/', TemplateView.as_view(template_name="index.html")),
     path('contact/', TemplateView.as_view(template_name="contact.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

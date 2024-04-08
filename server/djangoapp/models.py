61962b5f3dd08cd58ee5102d71817e743841ac82
Rey <mofiyinolawuyi.acl@gmail.com>
2024-04-08 15:20:37 -0400

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -17,7 +17,7 @@ class CarMake(models.Model):
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     # Many-to-One relationship
     name = models.CharField(max_length=100)
     CAR_TYPES = [
@@ -31,10 +31,10 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-    validators=[
+        validators=[
             MaxValueValidator(2023),
             MinValueValidator(2015)
-    ])
+        ])
     # Other fields as needed
 
     def __str__(self):
